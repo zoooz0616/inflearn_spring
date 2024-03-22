71a1b120d89be17d402c1bb95fc76112e89c54a1
eunji <zoooz0616@gmail.com>
2024-03-22 17:07:43 +0900

    [eunji] #add 순수 JDBC

--- demo/src/main/java/com/example/demo/SpringConfig.java
+++ demo/src/main/java/com/example/demo/SpringConfig.java
@@ -1,22 +1,31 @@
 package com.example.demo;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.example.demo.repository.JdbcMemberRepository;
 import com.example.demo.repository.MemberRepository;
-import com.example.demo.repository.MemoryMemberRepository;
 import com.example.demo.service.MemberService;
 
 @Configuration
 public class SpringConfig {
 
+	private DataSource dataSource;
+
+	public SpringConfig(DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
+
 	@Bean
 	public MemberService memberService() {
 		return new MemberService(memberRepository());
 	}
-	
+
 	@Bean
 	public MemberRepository memberRepository() {
-		return new MemoryMemberRepository();
+//		return new MemoryMemberRepository();
+		return new JdbcMemberRepository(dataSource);
 	}
 }
